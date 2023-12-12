2ffdf9636b0a53d59ab05241dd59af67fc9b7169
Mario Alvarado <maragile@espol.edu.ec>
2023-12-11 21:20:10 -0500

    continuomastarde

--- SistemaMercadoLibre.py
+++ SistemaMercadoLibre.py
@@ -1,10 +1,17 @@
-import pymysql
+# import pymysql
 
-miConexion = pymysql.connect(host="localhost", user='root', passwd= 'password123', db='rent_car')
-cur = miConexion.cursor()
-cur.execute("select VIN, Model from car")
+# miConexion = pymysql.connect(host="localhost", user='root', passwd= 'password123', db='rent_car')
+# cur = miConexion.cursor()
+# cur.execute("select VIN, Model from car")
 
-for VIN, Model in cur.fetchall():
-    print(VIN, Model)
+# for VIN, Model in cur.fetchall():
+#     print(VIN, Model)
 
-miConexion.close()
+# miConexion.close()
+
+def bienvenida() :
+    print("----- MERCADO LIBRE -----")
+
+bienvenida()
+
+
