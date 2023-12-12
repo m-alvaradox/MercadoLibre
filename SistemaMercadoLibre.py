362d8e808e23b1361599902a8174281d31de72b5
Mario Alvarado <96087936+m-alvaradox@users.noreply.github.com>
2023-12-11 20:41:13 -0500

    Add files via upload

---
+++ SistemaMercadoLibre.py
@@ -0,0 +1,10 @@
+import pymysql
+
+miConexion = pymysql.connect(host="localhost", user='root', passwd= 'password123', db='rent_car')
+cur = miConexion.cursor()
+cur.execute("select VIN, Model from car")
+
+for VIN, Model in cur.fetchall():
+    print(VIN, Model)
+
+miConexion.close()
