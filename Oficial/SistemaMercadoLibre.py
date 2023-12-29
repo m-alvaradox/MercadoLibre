b987d86c8eff134d997de60a761e79f52bd462b5
Mario Alvarado <maragile@espol.edu.ec>
2023-12-29 17:57:42 -0500

    implementacion inicio sesion

--- Oficial/SistemaMercadoLibre.py
+++ Oficial/SistemaMercadoLibre.py
@@ -1,12 +1,19 @@
 import pymysql
 
+# Conexion a la base de datos de mercadolibre
+mercadolibreconnection = pymysql.connect(host="servergroup3.mysql.database.azure.com", user='root', passwd= 'root', db='mercadolibre')
+cur = mercadolibreconnection.cursor()
+
 def imprimirMenuPrincipal():
   print("\n----- MERCADO LIBRE -----")
   print("Compra más facil y seguro")
-  print('1. Ver Publicaciones')
-  print('2. Ver Recientes Publicaciones 2023')
-  print('3. Mis Cupones')
-  print('4. Productos existentes de categoria Autos')
+  print('1. Iniciar sesión')
+  print('2. Crear Cuenta')
+  print('3. Ver Publicaciones')
+  print('4. Ver Recientes Publicaciones 2023')
+  print('5. Mis Cupones')
+  print('6. Productos existentes de categoria Autos')
+
   opcion = int(input('\nSeleccione una opcion: '))
 
   return opcion
@@ -53,26 +60,38 @@ def mostrarAccesoriosAutos(cursor):
           '\nSubcategoria: ',SUBCATEGORIA,
           '\n-----------------------------------\n')
 
+def IniciarSesion(cursor):
+   while True:
+      userName = input("Ingrese su nombre de usuario: ")
+      password = input("Ingrese su contraseña: ")
+      
+      cur.execute("SELECT USERID,PASS FROM USUARIO WHERE USERID = '"+userName+"'")
+      for USERID,PASS in cur.fetchall():
+         if(userName == USERID and password == PASS):
+            print("Inicio exitoso")
+            return
+         
+            
+               
 
-mercadolibreconnection = pymysql.connect(host="localhost", user='root', passwd= 'root', db='mercadolibre')
-cur = mercadolibreconnection.cursor()
 
-#Menu Principal Mercado Libre
 op = imprimirMenuPrincipal()
 
-# Mostrar todas las publicaciones
 
 if op == 1:
+   IniciarSesion(cur)
+
+if op == 3:
   mostrarPublicaciones(cur)
 
-elif op == 2:
+elif op == 4:
   mostrarPublicaciones2023(cur)
 
-elif op == 3:
+elif op == 5:
   user = input("Ingrese su nombre de usuario: ")
   mostrarCupones(cur,user)
 
-elif op == 4:
+elif op == 6:
   mostrarAccesoriosAutos(cur)
 
 
