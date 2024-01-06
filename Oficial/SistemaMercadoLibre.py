a94e82be962b8bc1ea2549e25dd5d6541b972d95
OwuenYagual <oayagual@espol.edu.ec>
2024-01-06 11:25:16 -0500

    Funcion registro de visita de publicacion

--- Oficial/SistemaMercadoLibre.py
+++ Oficial/SistemaMercadoLibre.py
@@ -897,6 +897,7 @@ def mostrarProductos():
       print("Categoria:",listaProductos[i][3])
       print("Subcategoria: ",listaProductos[i][4])
 
+# Publicaciones
 def crearpublicacion(user):
    print("\n--- VENDER ---\n")
    lusuarios = []
@@ -998,7 +999,11 @@ def crearpublicacion(user):
    mercadolibreconnection.commit()
    print("\nHas publicado tu venta")
 
-   
+#Visualizacion de publicaciones
+#Esta funcion debe ser llamada despues de abrir a detalle una publicacion de eleccion del user, recuperando el numero de
+def registrarVisualizacion(user,noPublicacion):
+   cur.execute("INSERT INTO VISUALIZACION_PUBLICACIONES (USERID, NOPUBLICACION, FECHA) VALUES ('"+user+"','"+noPublicacion+"','"+str(datetime.now)+"')")
+   mercadolibreconnection.commit()
 
 # Facturas
    
@@ -1026,7 +1031,6 @@ def mostrarFacturas(user):
 
 
 def mostrarFacturasEmitidas(user):
-   limpiarPantalla()
    print("--- FACTURAS EMITIDAS ---")
    cur.execute("SELECT FACTID, FECHA, DESCRIPCION, FACT.IDCLIENTE, FACT.IDORDEN, NOMBRE FROM PRODUCTO PROD JOIN ORDEN ORD USING(PRODUCTID) JOIN FACTURA FACT ON IDORDEN = ORDERID JOIN VENDEDOR V ON FACT.IDVENDEDOR = USERID WHERE FACT.IDVENDEDOR ='"+user+"'")
    resultado = cur.fetchall()
